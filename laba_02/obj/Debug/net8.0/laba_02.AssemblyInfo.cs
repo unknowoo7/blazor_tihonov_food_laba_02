650fbef550ba6641be7228747ddb407f7dc4bc53
unknowoo7 <kazak1995kazak@yandex.ru>
2025-01-08 16:09:51 +0300

    It`s working! Add export data in excel.

--- laba_02/obj/Debug/net8.0/laba_02.AssemblyInfo.cs
+++ laba_02/obj/Debug/net8.0/laba_02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c684d4478f145852415ac38884b82cd7f0c0af3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0a98c29108ff4b20e9417f902ad75c6da191be")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba_02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- laba_02/wwwroot/js/openfile.js
+++ laba_02/wwwroot/js/openfile.js
@@ -1,21 +1,10 @@
-function BlazorDownloadFile(filename, content) {
-    // thanks to Geral Barre : https://www.meziantou.net/generating-and-downloading-a-file-in-a-blazor-webassembly-application.htm 
-
-    // Create the URL
-    const file = new File([content], filename, { type: "application/octet-stream" });
-    const exportUrl = URL.createObjectURL(file);
-
-    // Create the <a> element and click on it
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.href = exportUrl;
-    a.download = filename;
-    a.target = "_self";
-    a.click();
-
-    // We don't need to keep the object url, let's release the memory
-    // On Safari it seems you need to comment this line... (please let me know if you know why)
-    URL.revokeObjectURL(exportUrl);
+function BlazorDownloadFile(fileName, mimeType, base64Data) {
+    const link = document.createElement('a');
+    link.href = `data:${mimeType};base64,${base64Data}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 function BlazorOpenFile(filename, content) {
